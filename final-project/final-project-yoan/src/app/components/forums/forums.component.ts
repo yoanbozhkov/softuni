442c8124a9d6a9a5627f3e32a192da94a5ded058
yoanbozhkov <yoanbozhkov1999@gmail.com>
2024-12-09 23:23:49 +0200

    Forums Component

---
+++ final-project/final-project-yoan/src/app/components/forums/forums.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forums',
+  standalone: true,
+  imports: [],
+  templateUrl: './forums.component.html',
+  styleUrl: './forums.component.css'
+})
+export class ForumsComponent {
+
+}
