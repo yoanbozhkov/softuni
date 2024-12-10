698fc9082e0c4869756b3de11c3a83be2a7b1640
yoanbozhkov <yoanbozhkov1999@gmail.com>
2024-12-10 17:09:58 +0200

    finalized backend service & controller

---
+++ final-project/API/src/controllers/tennisForumController.js
@@ -0,0 +1,151 @@
+const { isAuth } = require("../middleware/authMiddleware");
+const tennisForumService = require("../services/tennisForumService");
+
+const router = require("express").Router();
+
+// Get Latest Posts
+router.get("/latest", async (res) => {
+  try {
+    const latestPosts = await tennisForumService.getLatestPosts();
+    res.status(200).send(latestPosts);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+// Get Comments for a single Post
+router.get("/:id/comments", isAuth, async (req, res) => {
+  const postId = req.params.id;
+  try {
+    const commentsForPost = await tennisForumService.getCommentsForPost(postId);
+    res.status(200).send(commentsForPost);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+// Create a Post
+router.post("/", isAuth, async (req, res) => {
+  const { title, description } = req.body;
+  const userId = req.user._id;
+
+  try {
+    const createdPost = await tennisForumService.createPost({
+      title,
+      description,
+      postCreator: userId,
+    });
+
+    res.status(200).send(createdPost);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+// Create a Comment within a Post
+router.post("/:id/comment", isAuth, async (req, res) => {
+  const { content } = req.body;
+  const postId = req.params.id;
+  const userId = req.user._id;
+
+  try {
+    const createComment = await tennisForumService.createComment(
+      postId,
+      content,
+      userId
+    );
+
+    res.status(200).send(createComment);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+// Updates a Post
+router.put("/:id", isAuth, async (req, res) => {
+  const postId = req.params.id;
+  const { title, description } = req.body;
+
+  try {
+    const post = await tennisForumService.getPostById(postId);
+
+    if (req?.user._id != post.postCreator) {
+      res.status(401).send("Not Authorized");
+      return;
+    }
+
+    const updatedPost = await tennisForumService.updatePost(
+      postId,
+      title,
+      description
+    );
+
+    res.status(200).send(updatedPost);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+// Updates a Comment
+router.put("/comments/:id", isAuth, async (req, res) => {
+  const commentId = req.params.id;
+  const { content } = req.body;
+
+  try {
+    const comment = await tennisForumService.getCommentById(commentId);
+
+    if (req?.user._id != comment.commentCreator) {
+      res.status(401).send("Not Authorized");
+      return;
+    }
+
+    const updatedPost = await tennisForumService.updateComment(
+      commentId,
+      content
+    );
+
+    res.status(200).send(updatedPost);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+// Delete a Post
+router.delete("/:id", isAuth, async (req, res) => {
+  const postId = req.params.id;
+  try {
+    const post = await tennisForumService.getPostById(postId);
+    if (req?.user._id != post.postCreator) {
+      res.status(401).send("Not Authorized");
+      return;
+    }
+    const deletePost = await tennisForumService.deletePost(postId);
+    res.status(200).send(deletePost);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+// Delete a Comment
+router.delete("/:postId/comments/:id", isAuth, async (req, res) => {
+  const commentId = req.params.id;
+  const postId = req.params.postId;
+  try {
+    const comment = await tennisForumService.getCommentById(commentId);
+
+    if (req?.user._id != comment.commentCreator) {
+      res.status(401).send("Not Authorized");
+      return;
+    }
+
+    const deletedComment = await tennisForumService.deleteComment(
+      commentId,
+      postId
+    );
+    res.status(200).send(deletedComment);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+module.exports = tennisForumController = router;
--- final-project/API/src/services/tennisForumService.js
+++ final-project/API/src/services/tennisForumService.js
@@ -1,24 +1,36 @@
-const Post = require("../model/Posts");
-const Comment = require("../model/Comments");
+const Post = require("../model/Post");
+const Comment = require("../model/Comment");
 
 const getLatestPosts = async () => {
   return await Post.find().sort({ created_at: -1 }).limit(10).populate();
 };
 
 const getCommentsForPost = async (postId) => {
-  const comments = await Comment.find({ commentForPost: postId });
+  const comments = await Post.find({ postComments: postId });
   return comments;
 };
 
+const getCommentById = async (id) => {
+  return await Comment.findById(id);
+};
+
+const getPostById = async (id) => {
+  return await Post.findById(id);
+};
 const createPost = async (post) => {
   const createdPost = await Post.create({ ...post });
   return createdPost;
 };
 
-const createComment = async (postId, content) => {
+const createComment = async (postId, content, userId) => {
   const newComment = await Comment.create({
     content,
     commentForPost: postId,
+    commentCreator: userId,
+  });
+
+  await Post.findByIdAndUpdate(postId, {
+    $push: { postComments: newComment._id },
   });
   return newComment;
 };
@@ -43,20 +55,22 @@ const updateComment = async (commentId, newContent) => {
 };
 
 const deletePost = async (postId) => {
-  await Comment.deleteMany({ commentForPost: postId });
-
-  const post = await Post.findByIdAndDelete(postId);
-
-  return post;
+  const postDeleted = await Post.findByIdAndDelete(postId);
+  await Comment.deleteMany({ _id: { $in: [...postDeleted.postComments] } });
 };
 
-const deleteComment = async (commentId) => {
-  const comment = await Comment.findByIdAndDelete(commentId);
-  return comment;
+const deleteComment = async (commentId, postId) => {
+  await Comment.findByIdAndDelete(commentId);
+
+  await Post.findByIdAndUpdate(postId, {
+    $pull: { postComments: commentId },
+  });
 };
 
 const TennisForumService = {
   getLatestPosts,
+  getCommentById,
+  getPostById,
   createPost,
   updatePost,
   deletePost,
