e989f6eac35e4eb1cdeb67f5fd67d4a64a5e6c9b
yoanbozhkov <yoanbozhkov1999@gmail.com>
2024-12-10 21:44:28 +0200

    Routes

---
+++ final-project/final-project-yoan/src/app/routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AboutComponent } from './components/about/about.component';
+import { ForumsComponent } from './components/forums/forums.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
+import { CreateCommentComponent } from './components/create-comment/create-comment.component';
+
+export const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'forums', component: ForumsComponent },
+  { path: 'about', component: PostDetailsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'create-post', component: CreatePostComponent },
+  { path: 'create-comment', component: CreateCommentComponent },
+  { path: '**', redirectTo: 'home' },
+];
