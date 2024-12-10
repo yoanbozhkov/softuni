cf4d330a6288b2fa50bb6d72d9f25126a59545db
yoanbozhkov <yoanbozhkov1999@gmail.com>
2024-12-10 21:43:10 +0200

    Card Component

---
+++ final-project/final-project-yoan/src/app/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css',
+})
+export class CardComponent {
+  @Input() title: string = '';
+  @Input() description: string = '';
+}
