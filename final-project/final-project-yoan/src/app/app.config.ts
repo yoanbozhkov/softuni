ffd398ac5e474926c8d93c87cae521da06eca1c5
yoanbozhkov <yoanbozhkov1999@gmail.com>
2024-12-10 21:39:58 +0200

    main component

---
+++ final-project/final-project-yoan/src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
+import { appRoutes } from './routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
+    provideClientHydration(),
+  ],
+};
