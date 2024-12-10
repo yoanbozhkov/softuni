96451f2cd51e66ad1138baa1384aae93d7810c35
yoanbozhkov <yoanbozhkov1999@gmail.com>
2024-12-10 21:42:55 +0200

    Posts Component

--- final-project/final-project-yoan/src/app/components/forums/forums.component.ts
+++ final-project/final-project-yoan/src/app/components/forums/forums.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forums',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CommonModule, RouterLink],
   templateUrl: './forums.component.html',
-  styleUrl: './forums.component.css'
+  styleUrl: './forums.component.css',
 })
 export class ForumsComponent {
-
+  cards = [
+    { title: 'Card 1', description: 'This is description 1' },
+    { title: 'Card 2', description: 'This is description 2' },
+    { title: 'Card 3', description: 'This is description 3' },
+    { title: 'Card 4', description: 'This is description 4' },
+    { title: 'Card 5', description: 'This is description 5' },
+    { title: 'Card 6', description: 'This is description 6' },
+    { title: 'Card 7', description: 'This is description 7' },
+    { title: 'Card 8', description: 'This is description 8' },
+    { title: 'Card 9', description: 'This is description 9' },
+  ];
 }
