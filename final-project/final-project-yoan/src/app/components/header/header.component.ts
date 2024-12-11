e8ed2bc1eb5796ca0f8211e509d26d71a4ac3b97
yoanbozhkov <yoanbozhkov1999@gmail.com>
2024-12-11 20:02:21 +0200

    Header Component

--- final-project/final-project-yoan/src/app/components/header/header.component.ts
+++ final-project/final-project-yoan/src/app/components/header/header.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../../user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+
+  get username(): string {
+    return this.userService.user?.username || '';
+  }
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  logout() {
+    this.userService.logout().subscribe(() => {
+      this.router.navigate(['/home']);
+    });
+  }
+}
