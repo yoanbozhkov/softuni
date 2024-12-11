ec5db1d8f1ab1ec39935250b5a9d33219cbe8c32
yoanbozhkov <yoanbozhkov1999@gmail.com>
2024-12-11 15:17:51 +0200

    Landing Page & fixed routes

--- final-project/final-project-yoan/src/app/components/home-page/home-page.component.ts
+++ final-project/final-project-yoan/src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
