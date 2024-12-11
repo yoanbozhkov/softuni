9997dc3552372994fdcbea4d38caa42dd0261bd4
yoanbozhkov <yoanbozhkov1999@gmail.com>
2024-12-11 20:00:53 +0200

    Login & Register Component

--- final-project/final-project-yoan/src/app/components/login/login.component.ts
+++ final-project/final-project-yoan/src/app/components/login/login.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { DOMAINS } from '../../environment';
+import { UserService } from '../../user.service';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  domains = DOMAINS;
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  login(form: NgForm) {
+    if (form.invalid) {
+      console.error('Invalid Login Form!');
+      return;
+    }
+
+    const { email, password } = form.value;
+
+    this.userService.login(email, password).subscribe(() => {
+      this.router.navigate(['/themes']);
+    });
+  }
+}
--- final-project/final-project-yoan/src/app/components/register/register.component.ts
+++ final-project/final-project-yoan/src/app/components/register/register.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../../user.service';
+import { DOMAINS } from '../../environment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  domains = DOMAINS;
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  isFieldTextMissing(form: NgForm, controlName: string) {
+    return (
+      form.controls[controlName]?.touched &&
+      form.controls[controlName]?.errors?.['required']
+    );
+  }
+
+  isEmailNotValid(form: NgForm) {
+    return (
+      form.controls['email']?.touched &&
+      form.controls['email']?.errors?.['emailValidator']
+    );
+  }
+
+  isPasswordMismatch(form: NgForm) {
+    return (
+      form.controls['password']?.touched &&
+      form.controls['rePassword']?.touched &&
+      form.controls['password']?.value !== form.controls['rePassword']?.value
+    );
+  }
+
+  register(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    const { username, firstName, lastName, email, password, rePassword } =
+      form.value;
+
+    this.userService
+      .register(username!, firstName!, lastName!, email, password!, rePassword!)
+      .subscribe(() => {
+        this.router.navigate(['/post']);
+      });
+  }
+}
